bb3ad9b8cf7ada22cfd176e63328b026e6920017
SURYANSH8800 <suryanshpandey8800@gmail.com>
2025-01-06 18:29:43 +0530

    To check if a given character is a vowel or a consonant.

--- git.c
+++ git.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 void main(){
-    int n;
-    printf("ENTER THE NUMBER :");
-    scanf("%d",&n);
-    (n==0)?printf("ZERO"):((n>0)?printf("POSITIVE"):printf("NEGATIVE"));
+    char c;
+    printf("ENTER A LETTER TO CHECK : ");
+    scanf("%c",&c);
+    if(c == 'a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U'){
+        printf(" %c is a vowel",c);
+    }
+    else{
+        printf("%c is a consonant",c);
+    }
 }
